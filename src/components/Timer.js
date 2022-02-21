0708e1298a7bf4c159f44a8f3be1d3e19148a15b
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-02-20 20:08:40 -0600

    Update Timer.js

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function Timer() {
 
-  const [time, setTime] = useState('25')
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [displayMessage, setDisplayMessage] = useState(false);
 
-  return (<div>
-    {time}
-  </div>);
+  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  useEffect(() => {
+    setTimeout(function timer() {
+      if (seconds === 0) {
+        if (minutes !== 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        } else {
+          let minutes = displayMessage ? 24 : 4;
+
+          setMinutes(minutes);
+          setSeconds(59);
+          setDisplayMessage(!displayMessage);
+        }
+      } else {
+        setSeconds(seconds - 1)
+      }
+    }, 1000)
+  }, [seconds])
+
+  return (
+    <div className="pomodoro">
+      <div className="message">
+        {displayMessage && <div>Break time! You can start your next session in: </div>}
+      </div>
+      <div className="timer">
+        {timerMinutes}:{timerSeconds}
+      </div>
+    </div>);
 }
 
 export default Timer;
