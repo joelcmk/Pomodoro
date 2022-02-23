9d4b332d313fea00a71727a48c4ce2edcc81126f
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-02-22 19:17:28 -0600

    Added style and fixed timer function

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,41 +3,67 @@ import React, { useState, useEffect } from 'react';
 
 function Timer() {
 
-  const [minutes, setMinutes] = useState(25);
+  var [minutes, setMinutes] = useState(1);
   const [seconds, setSeconds] = useState(0);
   const [displayMessage, setDisplayMessage] = useState(false);
+  const [start, setStart] = useState(false);
 
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  const button = start ? 'START' : 'STOP';
 
   useEffect(() => {
-    setTimeout(function timer() {
-      if (seconds === 0) {
-        if (minutes !== 0) {
-          setSeconds(59);
-          setMinutes(minutes - 1);
-        } else {
-          let minutes = displayMessage ? 24 : 4;
+    let interval = setInterval(() => {
+      clearInterval(interval)
 
-          setMinutes(minutes);
-          setSeconds(59);
-          setDisplayMessage(!displayMessage);
+      if (start === false) {
+        clearInterval(interval)
+      } else
+        if (seconds === 0) {
+          if (minutes !== 0) {
+            setSeconds(59)
+            setMinutes(minutes - 1)
+          } else {
+            let minutes = displayMessage ? 24 : 4
+            let seconds = 59
+
+            setSeconds(seconds)
+            setMinutes(minutes)
+            setDisplayMessage(!displayMessage)
+          }
+        } else {
+          setSeconds(seconds - 1)
         }
-      } else {
-        setSeconds(seconds - 1)
-      }
     }, 1000)
-  }, [seconds])
+  }, [seconds, start])
+
+  const stop = () => {
+    setStart(false)
+  }
+
+  const startTimer = () => {
+    setStart(true);
+  }
 
   return (
     <div className="pomodoro">
-      <div className="message">
-        {displayMessage && <div>Break time! You can start your next session in: </div>}
+      <div className="timer-box">
+        <div className="message">
+          {displayMessage && <div>Break time! You can start your next session in: </div>}
+        </div>
+        <div className="timer">
+          {timerMinutes}:{timerSeconds}
+        </div>
+        <div className="button">
+          {start
+            ?
+            <button onClick={stop}>STOP</button>
+            :
+            <button onClick={startTimer} >START</button>
+          }
+        </div>
       </div>
-      <div className="timer">
-        {timerMinutes}:{timerSeconds}
-      </div>
-    </div>);
+    </div >);
 }
 
 export default Timer;
