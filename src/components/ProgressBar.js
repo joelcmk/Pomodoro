2cc580b4d755c25db83d3c53f2a33ad970fd377f
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-03-24 20:49:26 -0500

    Added progress bar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+
+function ProgressBar({ x, minutes, seconds }) {
+
+  const [porcentage, setPorcentage] = useState()
+
+  useEffect(() => {
+    const test = minutes * 60 + seconds
+    if (x === 1 || x === 3 || x === 5) {
+      setPorcentage(100 - (test * 100 / 1500))
+    } else if (x === 2 || x === 4) {
+      setPorcentage(100 - (test * 100 / 300))
+    } else if (x === 6) {
+      setPorcentage(100 - (test * 100 / 900))
+    }
+  }, [minutes, seconds, x])
+
+  return (
+    <div className="progress_bar">
+      <div className="progress"><div className="percentage" style={{ width: `${porcentage}%` }}></div></div>
+    </div>);
+}
+
+export default ProgressBar;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import ProgressBar from '../components/ProgressBar';
 
 
 function Timer() {
 
-  const [minutes, setMinutes] = useState(0);
+  const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
   const [displayMessage, setDisplayMessage] = useState(false);
   const [start, setStart] = useState(false);
   const [x, setX] = useState(1); // X = Cycle number 
   const [background, setBackground] = useState('#D8564F');
   const [color, setColor] = useState('red');
+  const [porcentage, setPorcentage] = useState()
 
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
@@ -73,8 +75,8 @@ function Timer() {
       } else {
         if (seconds === 0) {
           if (minutes !== 0) {
-            setSeconds(59)
             setMinutes(minutes - 1)
+            setSeconds(59)
           } else {
             setStart(!start);
             setX(x + 1)
@@ -103,16 +105,9 @@ function Timer() {
     setTimeout(() => {
       setX(x + 1);
     }, 1000);
-
   }
 
-  const message = () => {
-    if (start) {
-      return <div>Time to work!</div>;
-    } else if (start === false) {
-      return <div>Time to relax!</div>;
-    }
-  }
+
 
   return (
     <div className="pomodoro">
@@ -130,10 +125,11 @@ function Timer() {
           <div className={`skip ${color}`} onClick={skip}>></div>
         </div>
       </div>
-      <div className="message">
-        {displayMessage && <div>Break time! You can start your next session in: </div>}
-        {}
+      <div className="progress_box">
+
+        <ProgressBar x={x} minutes={minutes} seconds={seconds} />
       </div>
+      {displayMessage && <div>Break time! You can start your next session in: </div>}
     </div >);
 }
 
